4139cf0ce19c62e2d2949d6bf1258376cc780d1f
Jules THUYSBAERT <jthuysba@e2r3p13.clusters.42paris.fr>
2022-06-08 15:37:26 +0200

    clean2

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 11:51:46 by jthuysba          #+#    #+#             */
-/*   Updated: 2022/05/31 15:07:52 by jthuysba         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:36:52 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
+# gitinclude <unistd.h>
 # include <string.h>
 # include <stdlib.h>
 
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strcpy(char *dest, const char *src);
 size_t	ft_strlen(const char *str);
 
